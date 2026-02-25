2e00bf6e6b571b296b86775144af5bc492a2da55
Rudransh rai <rudranshrai08@gmail.com>
2026-02-25 16:44:49 +0530

    Add files via upload

---
+++ Sealedp/MainSealed.java
@@ -0,0 +1,24 @@
+sealed class Shape permits Circle, Rectangle  {
+
+
+
+}
+final class Circle extends Shape {
+
+}
+
+ non-sealed  class Rectangle extends Shape {
+
+}
+
+
+
+
+public class MainSealed {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
